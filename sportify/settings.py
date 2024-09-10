8bd49040a68acacb03aaa121011c6d362909a9ea
Fauzan <fauzanputra0703@gmail.com>
2024-09-10 21:46:24 +0700

    fix typo

--- sportify/settings.py
+++ sportify/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-s
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     # Menunjuk ke direktori `main/static/`
     os.path.join(BASE_DIR, 'main/static/main'),
